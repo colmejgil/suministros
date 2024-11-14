840dffe698c6737d6d6e114e614c455bda9f8e9e
jose.colmenares.gil <jose.colmenares.gil@gmail.com>
2024-11-14 23:27:16 +0000

    Incorporar Componentes de Navigation & Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { NavigationComponent } from './navigation/navigation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatDividerModule, MatFormFieldModule, MatInputModule],
+  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatDividerModule, MatFormFieldModule, MatInputModule, NavigationComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'Suministros';
 }
