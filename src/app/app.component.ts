0b8dbb1ab1c8bdb94918124aa735259249d50a77
jose.colmenares.gil <jose.colmenares.gil@gmail.com>
2024-11-15 00:56:28 +0000

    Agregar Ruta por Defecto al DashBoard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { NavigationComponent } from './navigation/navigation.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatDividerModule, MatFormFieldModule, MatInputModule, NavigationComponent, DashboardComponent],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
